05ac08ca5045923298324498a5d3d9ff148c42fd
Shaya <shayaajz@gmail.com>
2017-10-26 20:41:41 +0300

    ger

--- client/react/src/components/WooStandard.js
+++ client/react/src/components/WooStandard.js
@@ -27,13 +27,17 @@ export default class WooStandard extends React.Component {
             <div className="card">
                 <div className="container standard-layout">
                     <p id="agreed-msg">They have already agreed to meet {meetCount.met}/{meetCount.total} of your standards:</p>
-                    <p className="single-standard standard-approve">Preferred Salary</p>
-                    <p className="single-standard standard-approve">Relocation</p>
-                    <p className="single-standard standard-approve">Company Specialty</p>
-                    <p className="single-standard standard-approve">Specific company</p>
-                    <p className="single-standard standard-disapprove">Ice cream daily</p>
-                    <p className="single-standard standard-disapprove">Fifa play station</p>
-
+                    
+                    {
+                        this.props.standards.map((entry, index) => (
+                            entry.met 
+                            ?
+                            <p className="single-standard standard-approve">{entry.name}</p> 
+                            :
+                            <p className="single-standard standard-disapprove">{entry.name}</p> 
+                          ))
+                    }
+                    
                     <div id="note-from-company">
                         <h1>Notes from Google:</h1>
                         <h2>We can relocate you to our Berlin office but with your current job position. There will be an option for
