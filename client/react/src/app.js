79739966c207d5fb5fc5d142abd0dacc33379514
Shaya <shayaajz@gmail.com>
2017-10-29 00:23:17 +0200

    remove redundant files

--- client/react/src/app.js
+++ client/react/src/app.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+
 import AppRouter, { history } from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import configureStore from './store/configureStore';
 let firstRender = true;
-const store = configureStore();
+
 const jsx = (
-  <Provider store={store}>
+
     <AppRouter />
-  </Provider>
+
 );
 let hasRendered = false;
 const renderApp = () => {
